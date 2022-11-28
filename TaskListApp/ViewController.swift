a6574abdfba8b619b7151d17088f14bee0824849
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-11-28 22:06:10 +0300

    Initial Commit

---
+++ TaskListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListApp
+//
+//  Created by jopootrivatel on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
